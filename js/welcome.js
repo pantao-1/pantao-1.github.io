577bb8c2299df2d8ef39b2262796a975e0cab65b
32941 <32994152829@qq.com>
2025-12-09 15:27:53 +0800

    Site updated: 2025-12-09 15:27:52

--- js/welcome.js
+++ js/welcome.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(121.01, 31.38, ipLocation.result.location.lng, ipLocation.result.location.lat); //这里记得换成自己的经纬度
+    let dist = getDistance(120.98181, 31.38475, ipLocation.result.location.lng, ipLocation.result.location.lat); //这里记得换成自己的经纬度
     let pos = ipLocation.result.ad_info.nation;
     let ip;
     let posdesc;
